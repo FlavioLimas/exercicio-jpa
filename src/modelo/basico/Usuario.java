b4229d2647c281195c2809bb1549ff4dfde429fb
Flavio Lima <flaviolima.s@live.com>
2021-07-12 20:10:09 -0300

    definindo construtor com atributos

--- src/modelo/basico/Usuario.java
+++ src/modelo/basico/Usuario.java
@@ -4,4 +4,11 @@ public class Usuario {
     private Long id;
     private String nome;
     private String email;
+    
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    
 }
