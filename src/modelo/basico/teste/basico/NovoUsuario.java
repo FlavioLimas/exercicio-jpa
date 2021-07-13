fd18e2f8824edd33487d180b66ad0ae9fa1fe6b4
Flavio Lima <flaviolima.s@live.com>
2021-07-12 21:48:28 -0300

    cadastrando usuarios

--- src/modelo/basico/teste/basico/NovoUsuario.java
+++ src/modelo/basico/teste/basico/NovoUsuario.java
@@ -15,9 +15,17 @@ public class NovoUsuario {
 
         Usuario novoUsuario = new Usuario("Andre", "andre@lanche.com.br");
         Usuario novoUsuario1 = new Usuario("Rodrigo", "rodrigo@lanche.com.br");
+        Usuario novoUsuario2 = new Usuario("Carlos", "carlos@lanche.com.br");
+        Usuario novoUsuario3 = new Usuario("Arthur", "arthur@lanche.com.br");
+        Usuario novoUsuario4 = new Usuario("Djalma", "djalma@lanche.com.br");
+        Usuario novoUsuario5 = new Usuario("Lulu", "lulu@lanche.com.br");
         em.getTransaction().begin();
         em.persist(novoUsuario);
         em.persist(novoUsuario1);
+        em.persist(novoUsuario2);
+        em.persist(novoUsuario3);
+        em.persist(novoUsuario4);
+        em.persist(novoUsuario5);
         em.getTransaction().commit();
 
         em.close();
