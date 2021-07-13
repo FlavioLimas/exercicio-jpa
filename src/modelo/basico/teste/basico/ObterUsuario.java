d9f5742de2e64f8807ca427c16a20cbce03fb595
Flavio Lima <flaviolima.s@live.com>
2021-07-13 20:34:23 -0300

    definindo fabrica e entityManager para acesso DB

--- src/modelo/basico/teste/basico/ObterUsuario.java
+++ src/modelo/basico/teste/basico/ObterUsuario.java
@@ -1,8 +1,15 @@
 package modelo.basico.teste.basico;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ObterUsuario {
     
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
         
     }
 }
