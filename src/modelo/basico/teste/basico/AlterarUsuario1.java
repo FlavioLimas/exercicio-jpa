4a18305a8aa5eaa125a95d4f68041d1aaf9304a4
Flavio Lima <flaviolima.s@live.com>
2021-07-15 21:34:53 -0300

    definindo modelo de alteracao JPA

---
+++ src/modelo/basico/teste/basico/AlterarUsuario1.java
@@ -0,0 +1,17 @@
+package modelo.basico.teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AlterarUsuario1 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory eManagerFactory = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager entityManager = eManagerFactory.createEntityManager();
+
+        entityManager.close();
+        eManagerFactory.close();
+    }
+    
+}
