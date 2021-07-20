51bef9557d46c4ba7483d0fe46d1937ec875551a
Flavio Lima <flaviolima.s@live.com>
2021-07-20 20:15:47 -0300

    definindo entity e autoincrement

--- src/modelo/basico/Produto.java
+++ src/modelo/basico/Produto.java
@@ -1,13 +1,21 @@
 package modelo.basico;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private Double preco;
 
     public Produto() {
-        
+
     }
 
     public Produto(String nome, Double preco) {
