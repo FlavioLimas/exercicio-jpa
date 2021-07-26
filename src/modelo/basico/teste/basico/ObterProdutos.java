6bfa5ada735c6a3ba18aa225986e76423e4eaa85
Flavio Lima <flaviolima.s@live.com>
2021-07-26 20:16:58 -0300

    Definindo List

--- src/modelo/basico/teste/basico/ObterProdutos.java
+++ src/modelo/basico/teste/basico/ObterProdutos.java
@@ -1,9 +1,15 @@
 package modelo.basico.teste.basico;
 
+import java.util.List;
+
+import infra.ProdutoDAO;
+import modelo.basico.Produto;
+
 public class ObterProdutos {
 
     public static void main(String[] args) {
-        ProdutoDAO
+        ProdutoDAO dao = new ProdutoDAO();
+        List<Produto> produto = dao.obterTodos();
     }
     
 }
