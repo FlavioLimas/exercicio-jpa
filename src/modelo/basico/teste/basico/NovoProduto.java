58dffa590e3d931b4ffe0c4339a61a3bb6a68e7a
Flavio Lima <flaviolima.s@live.com>
2021-07-26 20:07:18 -0300

    inserindo produtos

--- src/modelo/basico/teste/basico/NovoProduto.java
+++ src/modelo/basico/teste/basico/NovoProduto.java
@@ -5,9 +5,11 @@ import modelo.basico.Produto;
 
 public class NovoProduto {
     public static void main(String[] args) {
-        Produto produto = new Produto("Caneta", 7.45);
+        Produto produto = new Produto("Monitor 23", 789.98);
 
         DAO<Produto> dao = new DAO<>(Produto.class);
-        dao.abrirTransacao().incluir(produto).fecharTransacao().fechar();
+        dao.incluirAtomico(produto).fechar();
+
+        System.out.println("Id do produto: " + produto.getId());
     }
 }
