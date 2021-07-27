22b15bc52b6e35c4e99d116eceb931661b4b6e73
Flavio Lima <flaviolima.s@live.com>
2021-07-27 20:02:41 -0300

    na mesma transacao ele resolve a dependencia

--- src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
+++ src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
@@ -6,14 +6,14 @@ import modelo.umparaum.Cliente;
 
 public class NovoClienteAssento1 {
     public static void main(String[] args) {
-        Assento assento = new Assento("16C");
-        Cliente cliente = new Cliente("Ana", assento);
+        Assento assento = new Assento("8C");
+        Cliente cliente = new Cliente("Carlos", assento);
 
         DAO<Object> dao = new DAO<>();
 
         dao.abrirTransacao()
-            .incluir(assento)
             .incluir(cliente)
+            .incluir(assento)
             .fecharTransacao()
             .fechar();
     }
