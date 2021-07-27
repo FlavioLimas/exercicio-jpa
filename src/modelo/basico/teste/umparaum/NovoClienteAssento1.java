c1e4a2060658d7e6a5823ef23db7d897d735330e
Flavio Lima <flaviolima.s@live.com>
2021-07-27 20:03:20 -0300

    Retornando para ordem correta

--- src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
+++ src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
@@ -6,14 +6,14 @@ import modelo.umparaum.Cliente;
 
 public class NovoClienteAssento1 {
     public static void main(String[] args) {
-        Assento assento = new Assento("8C");
-        Cliente cliente = new Cliente("Carlos", assento);
+        Assento assento = new Assento("23F");
+        Cliente cliente = new Cliente("Bia", assento);
 
         DAO<Object> dao = new DAO<>();
 
         dao.abrirTransacao()
-            .incluir(cliente)
             .incluir(assento)
+            .incluir(cliente)
             .fecharTransacao()
             .fechar();
     }
