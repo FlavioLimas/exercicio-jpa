db18b60949c2540f1d899d446a5d18b3de252c19
Flavio Lima <flaviolima.s@live.com>
2021-07-27 20:42:23 -0300

    inserindo mais registros

--- src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
+++ src/modelo/basico/teste/umparaum/NovoClienteAssento1.java
@@ -6,8 +6,8 @@ import modelo.umparaum.Cliente;
 
 public class NovoClienteAssento1 {
     public static void main(String[] args) {
-        Assento assento = new Assento("23F");
-        Cliente cliente = new Cliente("Bia", assento);
+        Assento assento = new Assento("16A");
+        Cliente cliente = new Cliente("Carlos", assento);
 
         DAO<Object> dao = new DAO<>();
 
