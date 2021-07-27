7034f5df6837745a7b8e8cc526ba2251e3bdee6a
Flavio Lima <flaviolima.s@live.com>
2021-07-27 20:52:07 -0300

    definindo uma nova classe

---
+++ src/modelo/basico/teste/umparaum/NovoClienteAssento2.java
@@ -0,0 +1,14 @@
+package modelo.basico.teste.umparaum;
+
+import infra.DAO;
+import modelo.umparaum.Assento;
+import modelo.umparaum.Cliente;
+
+public class NovoClienteAssento2 {
+    public static void main(String[] args) {
+        Assento assento = new Assento("4D");
+        Cliente cliente = new Cliente("Rodrigo", assento);
+        DAO<Cliente> dao = new DAO<>(Cliente.class);
+        dao.incluirAtomico(cliente);
+    }
+}
