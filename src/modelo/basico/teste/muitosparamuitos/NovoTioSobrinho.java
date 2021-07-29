55602eba7fc87b655f016edb394a09097e3128e6
Flavio Lima <flaviolima.s@live.com>
2021-07-29 20:28:26 -0300

    implementacao de classas Sobrinho e Tio

--- src/modelo/basico/teste/muitosparamuitos/NovoTioSobrinho.java
+++ src/modelo/basico/teste/muitosparamuitos/NovoTioSobrinho.java
@@ -1,14 +1,37 @@
 package modelo.basico.teste.muitosparamuitos;
 
+import infra.DAO;
 import modelo.basico.muitosparamuitos.Sobrinho;
 import modelo.basico.muitosparamuitos.Tio;
 
 public class NovoTioSobrinho {
     public static void main(String[] args) {
         Tio tia1 = new Tio("Maria");
-        Tio tia2 = new Tio("João");
+        Tio tio2 = new Tio("João");
 
         Sobrinho sobrinho1 = new Sobrinho("Davi");
-        Sobrinho sobrinho2 = new Sobrinho("Ana");
+        Sobrinho sobrinha2 = new Sobrinho("Ana");
+
+        tia1.getSobrinhos().add(sobrinho1);
+        sobrinho1.getTios().add(tia1);
+        
+        tia1.getSobrinhos().add(sobrinha2);
+        sobrinha2.getTios().add(tia1);
+        
+        tio2.getSobrinhos().add(sobrinho1);
+        sobrinho1.getTios().add(tio2);
+        
+        tio2.getSobrinhos().add(sobrinha2);
+        sobrinha2.getTios().add(tio2);
+
+        DAO<Object> dao = new DAO<>();
+        dao.abrirTransacao()
+            .incluir(tia1)
+            .incluir(tio2)
+            .incluir(sobrinho1)
+            .incluir(sobrinha2)
+            .fecharTransacao()
+            .fechar();
+
     }
 }
