a4cb210c57e09a94ac1bad6ad438c82dd9a2a88d
Flavio Lima <flaviolima.s@live.com>
2021-07-29 17:20:05 -0300

    fetch padrao conforme relacionamento

--- src/modelo/umparamuitos/ItemPedido.java
+++ src/modelo/umparamuitos/ItemPedido.java
@@ -2,6 +2,7 @@ package modelo.umparamuitos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,7 +22,7 @@ public class ItemPedido {
     
     @ManyToOne
     private Pedido pedido;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private Produto produto;
     public ItemPedido(Integer quantidade, Pedido pedido, Produto produto) {
         this.setQuantidade(quantidade);
