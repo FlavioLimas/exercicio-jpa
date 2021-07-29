8375539d137bb0d1a90e72400a5c6662fd746aab
Flavio Lima <flaviolima.s@live.com>
2021-07-28 21:15:06 -0300

    definicoes

--- src/modelo/umparamuitos/Pedido.java
+++ src/modelo/umparamuitos/Pedido.java
@@ -2,7 +2,15 @@ package modelo.umparamuitos;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     public Long getId() {
         return id;
