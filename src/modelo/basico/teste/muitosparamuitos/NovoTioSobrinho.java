ad4174461f71e57ae1abc7bf4cbb524564ea80f9
Flavio Lima <flaviolima.s@live.com>
2021-07-29 20:06:21 -0300

    definicao de classe de teste das entitys

---
+++ src/modelo/basico/teste/muitosparamuitos/NovoTioSobrinho.java
@@ -0,0 +1,14 @@
+package modelo.basico.teste.muitosparamuitos;
+
+import modelo.basico.muitosparamuitos.Sobrinho;
+import modelo.basico.muitosparamuitos.Tio;
+
+public class NovoTioSobrinho {
+    public static void main(String[] args) {
+        Tio tia1 = new Tio("Maria");
+        Tio tia2 = new Tio("João");
+
+        Sobrinho sobrinho1 = new Sobrinho("Davi");
+        Sobrinho sobrinho2 = new Sobrinho("Ana");
+    }
+}
