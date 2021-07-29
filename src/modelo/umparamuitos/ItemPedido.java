55c1f0726ddb4782b661267a23bc3cdf4a947172
Flavio Lima <flaviolima.s@live.com>
2021-07-28 21:06:19 -0300

    alteracoes da entity

---
+++ src/modelo/umparamuitos/ItemPedido.java
@@ -0,0 +1,63 @@
+package modelo.umparamuitos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import modelo.basico.Produto;
+
+@Entity
+public class ItemPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantidade;
+    private Double preco;
+    
+    @ManyToOne
+    private Pedido pedido;
+    @ManyToOne
+    private Produto produto;
+    public ItemPedido(Integer quantidade, Pedido pedido, Produto produto) {
+        this.setQuantidade(quantidade);
+        this.setPedido(pedido);
+        this.setProduto(produto);
+    }
+    public ItemPedido() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public Pedido getPedido() {
+        return pedido;
+    }
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        if (produto != null && this.preco == null) {
+            this.setPreco(produto.getPreco());
+        }
+        this.produto = produto;
+    }
+}
